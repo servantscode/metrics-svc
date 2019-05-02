8a23b3ec1131450f7f45db81c86892e20c021f5d
Greg Leitheiser <greg@servantscode.org>
2019-05-02 11:42:29 -0500

    Donation metric download

--- src/main/java/org/servantscode/metrics/rest/PlegeMetricsSvc.java
+++ src/main/java/org/servantscode/metrics/rest/PlegeMetricsSvc.java
@@ -2,16 +2,19 @@ package org.servantscode.metrics.rest;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.servantscode.commons.rest.ReportListStreamingOutput;
 import org.servantscode.commons.rest.SCServiceBase;
 import org.servantscode.metrics.MonthlyDonations;
 import org.servantscode.metrics.PledgeMetricsResponse;
 import org.servantscode.metrics.db.PledgeMetricsDB;
 
 import javax.ws.rs.*;
-
+import javax.ws.rs.core.Response;
 import java.util.List;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static javax.ws.rs.core.MediaType.TEXT_PLAIN;
+import static org.servantscode.commons.rest.AdditionalMediaTypes.TEXT_CSV;
 
 @Path("/metrics/pledges")
 public class PlegeMetricsSvc extends SCServiceBase {
@@ -29,25 +32,39 @@ public class PlegeMetricsSvc extends SCServiceBase {
         }
     }
 
-    @GET @Path("/monthly") @Produces(APPLICATION_JSON)
-    public List<MonthlyDonations> getMonthlyDonations(@QueryParam("months") @DefaultValue("13") int months) {
-        verifyUserAccess("donation.metrics");
+    @GET @Path("/monthly") @Produces({APPLICATION_JSON, TEXT_PLAIN, TEXT_CSV})
+    public Response getMonthlyDonations(@QueryParam("months") @DefaultValue("13") int months,
+                                        @HeaderParam("Accept") String responseType) {
+
+        boolean generateCSV = responseType.equals(TEXT_PLAIN) || responseType.equals("text/csv");
+        verifyUserAccess(generateCSV? "donation.export": "donation.metrics");
+
         try {
             LOG.debug("Retrieving monthly donation metrics.");
-            return new PledgeMetricsDB().getMonthlyDonations(months);
+            List<MonthlyDonations> donations = new PledgeMetricsDB().getMonthlyDonations(months);
+
+            Object retVal = generateCSV? new ReportListStreamingOutput<>(donations): donations;
+            return Response.ok(retVal).build();
         } catch (Throwable t) {
             LOG.error("Failed to generate monthly donation metrics.", t);
             throw t;
         }
     }
 
-    @GET @Path("/monthly/fund/{fundId}") @Produces(APPLICATION_JSON)
-    public List<MonthlyDonations> getMonthlyDonationsByFund(@PathParam("fundId") int fundId,
-                                                            @QueryParam("months") @DefaultValue("13") int months) {
-        verifyUserAccess("donation.metrics");
+    @GET @Path("/monthly/fund/{fundId}") @Produces({APPLICATION_JSON, TEXT_PLAIN, TEXT_CSV})
+    public Response getMonthlyDonationsByFund(@PathParam("fundId") int fundId,
+                                              @QueryParam("months") @DefaultValue("13") int months,
+                                              @HeaderParam("Accept") String responseType) {
+
+        boolean generateCSV = responseType.equals(TEXT_PLAIN) || responseType.equals("text/csv");
+        verifyUserAccess(generateCSV? "donation.export": "donation.metrics");
+
         try {
             LOG.debug("Retrieving monthly donation metrics.");
-            return new PledgeMetricsDB().getMonthlyDonationsForFund(months, fundId);
+            List<MonthlyDonations> donations = new PledgeMetricsDB().getMonthlyDonationsForFund(months, fundId);
+
+            Object retVal = generateCSV? new ReportListStreamingOutput<>(donations): donations;
+            return Response.ok(retVal).build();
         } catch (Throwable t) {
             LOG.error("Failed to generate monthly donation metrics.", t);
             throw t;
